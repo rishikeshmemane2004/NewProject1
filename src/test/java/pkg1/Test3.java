9e9163aa3b7a74e9d56190d6b44fbdc0212fc094
rishikeshmemane2004 <53008391+rishikeshmemane2004@users.noreply.github.com>
2023-06-21 19:33:00 +0530

    Create Test3.java

---
+++ src/test/java/pkg1/Test3.java
@@ -0,0 +1,14 @@
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is method from test3");
+	  System.out.println("this is method from test3");
+	  System.out.println("this is method from test3");
+	  System.out.println("this is method from test3");
+	  
+  
+  }
+
+  
+}
